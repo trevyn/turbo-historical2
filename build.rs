a05631066560144d189e567fb302f68b7e45b3da
trevyn <trevyn-git@protonmail.com>
2021-10-03 13:20:50 -0400

    add rclone and bmff

---
+++ build.rs
@@ -0,0 +1,12 @@
+fn main() {
+ let triple = std::env::var("TARGET").unwrap();
+ if triple != "wasm32-unknown-unknown" {
+  println!("cargo:rustc-link-search=rclone/build");
+  println!("cargo:rustc-link-arg-bins=-lrclone-{}", triple);
+  println!("cargo:rustc-link-arg-bins=-framework");
+  println!("cargo:rustc-link-arg-bins=CoreFoundation");
+  println!("cargo:rustc-link-arg-bins=-framework");
+  println!("cargo:rustc-link-arg-bins=Security");
+  println!("cargo:rerun-if-changed=rclone/build/librclone-{}.a", triple);
+ }
+}
--- src/server.rs
+++ src/server.rs
@@ -13,7 +13,7 @@ struct Opts {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
  #[derive(rust_embed::RustEmbed)]
  #[folder = "build"]
  struct Frontend;
@@ -25,6 +25,12 @@ async fn main() {
  log::info!("info enabled");
  log::debug!("debug enabled");
  log::trace!("trace enabled");
+
+ let cstring = CString::new("".to_owned()).unwrap();
+
+ unsafe {
+  GoListJSON(cstring.as_ptr());
+ }
 
  match (opts.key_path, opts.cert_path) {
   (Some(key_path), Some(cert_path)) => {
@@ -43,4 +49,81 @@ async fn main() {
   }
   _ => eprintln!("Both key-path and cert-path must be specified for HTTPS."),
  }
+
+ Ok(())
+}
+
+use four_cc::FourCC;
+use nom::bytes::streaming::take;
+use nom::number::streaming::{be_u32, be_u64};
+use nom::IResult;
+use std::convert::TryInto;
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_longlong, c_uchar};
+
+#[allow(dead_code, non_snake_case)]
+extern "C" {
+ fn GoListJSON(path: *const c_char);
+ fn GoFetchFiledata(path: *const c_char, startbytepos: c_longlong, endbytepos: c_longlong);
+}
+
+#[derive(Debug)]
+struct MediaBox {
+ size: u64,
+ ty: FourCC,
+}
+
+impl MediaBox {
+ fn parse(i: &[u8]) -> IResult<&[u8], MediaBox> {
+  let (i, size) = be_u32(i)?;
+  let (i, ty) = take(4usize)(i)?;
+  let ty: FourCC = ty.try_into().unwrap();
+  let (i, size) = match size {
+   1 => be_u64(i)?,
+   _ => (i, size.into()),
+  };
+
+  Ok((i, MediaBox { size, ty }))
+ }
+}
+
+/// Receive an array of File entries from Go and insert into turbosql
+/// # Safety
+/// `json` must be a valid pointer to valid C string until this function returns.
+#[no_mangle]
+extern "C" fn rust_insert_files_from_go(json: *const c_char) {
+ let c_str = unsafe { CStr::from_ptr(json) };
+ let string = c_str.to_str().unwrap().to_owned();
+ dbg!(string);
+
+ // let mut sender = RESPONSE_TX_CHANNEL.lock().unwrap().clone().unwrap();
+
+ // tokio::spawn(async move {
+ //  sender.send(string).await.unwrap();
+ // });
+}
+
+/// Receive a Filecache entry from Go and insert into turbosql
+/// buf is only valid until function return, must be copied
+#[no_mangle]
+extern "C" fn rust_insert_filecache_from_go(
+ json: *const c_char,
+ buf: *const c_uchar,
+ len: c_longlong,
+) {
+ let c_str = unsafe { CStr::from_ptr(json) };
+ let str = c_str.to_str().unwrap();
+
+ dbg!(str);
+
+ // log::info!("rust_insert_filecache_from_go: {:#?}", str);
+
+ // let mut fc: FileCache = serde_json::from_str(str).unwrap();
+
+ // log::info!("rust_insert_filecache_from_go fc: {:#?}", fc);
+
+ // let slice = unsafe { std::slice::from_raw_parts(buf, len as usize) };
+ // fc.bytes = Some(slice.to_vec());
+
+ // fc.insert().unwrap();
 }
