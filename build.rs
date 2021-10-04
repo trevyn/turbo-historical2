1ea3b565c3276b1cc697a11cd576900f0c38401e
trevyn <trevyn-git@protonmail.com>
2021-10-04 12:45:35 -0400

    only do `-framework`s for darwin

--- build.rs
+++ build.rs
@@ -3,10 +3,12 @@ fn main() {
  if triple != "wasm32-unknown-unknown" {
   println!("cargo:rustc-link-search=rclone/build");
   println!("cargo:rustc-link-arg-bins=-lrclone-{}", triple);
-  println!("cargo:rustc-link-arg-bins=-framework");
-  println!("cargo:rustc-link-arg-bins=CoreFoundation");
-  println!("cargo:rustc-link-arg-bins=-framework");
-  println!("cargo:rustc-link-arg-bins=Security");
+  if triple.ends_with("darwin") {
+   println!("cargo:rustc-link-arg-bins=-framework");
+   println!("cargo:rustc-link-arg-bins=CoreFoundation");
+   println!("cargo:rustc-link-arg-bins=-framework");
+   println!("cargo:rustc-link-arg-bins=Security");
+  }
   println!("cargo:rerun-if-changed=rclone/build/librclone-{}.a", triple);
  }
 }
