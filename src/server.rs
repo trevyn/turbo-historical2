a0226eb8f224f7ea197d081aeffb48110b072aa4
trevyn <trevyn-git@protonmail.com>
2021-10-16 15:37:43 -0400

    format

--- src/server.rs
+++ src/server.rs
@@ -78,41 +78,41 @@ async fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
   async move {
    loop {
     session.with_torrents(|torrents| {
-              for (idx, torrent) in torrents.iter().enumerate() {
-                  match &torrent.state {
-                      librqbit::session::ManagedTorrentState::Initializing => {
-                          log::info!("[{}] initializing", idx);
-                      },
-                      librqbit::session::ManagedTorrentState::Running(handle) => {
-                          let peer_stats = handle.torrent_state().peer_stats_snapshot();
-                          let stats = handle.torrent_state().stats_snapshot();
-                          let speed = handle.speed_estimator();
-                          let total = stats.total_bytes;
-                          let progress = stats.total_bytes - stats.remaining_bytes;
-                          let downloaded_pct = if stats.remaining_bytes == 0 {
-                              100f64
-                          } else {
-                              (progress as f64 / total as f64) * 100f64
-                          };
-                          log::info!(
-                              "[{}]: {:.2}% ({:.2}), down speed {:.2} Mbps, fetched {}, remaining {:.2} of {:.2}, uploaded {:.2}, peers: {{live: {}, connecting: {}, queued: {}, seen: {}}}",
-                              idx,
-                              downloaded_pct,
-                              SF::new(progress),
-                              speed.download_mbps(),
-                              SF::new(stats.fetched_bytes),
-                              SF::new(stats.remaining_bytes),
-                              SF::new(total),
-                              SF::new(stats.uploaded_bytes),
-                              peer_stats.live,
-                              peer_stats.connecting,
-                              peer_stats.queued,
-                              peer_stats.seen,
-                          );
-                      },
-                  }
-              }
-          });
+     for (idx, torrent) in torrents.iter().enumerate() {
+      match &torrent.state {
+       librqbit::session::ManagedTorrentState::Initializing => {
+        log::info!("[{}] initializing", idx);
+       },
+       librqbit::session::ManagedTorrentState::Running(handle) => {
+        let peer_stats = handle.torrent_state().peer_stats_snapshot();
+        let stats = handle.torrent_state().stats_snapshot();
+        let speed = handle.speed_estimator();
+        let total = stats.total_bytes;
+        let progress = stats.total_bytes - stats.remaining_bytes;
+        let downloaded_pct = if stats.remaining_bytes == 0 {
+         100f64
+        } else {
+         (progress as f64 / total as f64) * 100f64
+        };
+        log::info!(
+         "[{}]: {:.2}% ({:.2}), down speed {:.2} Mbps, fetched {}, remaining {:.2} of {:.2}, uploaded {:.2}, peers: {{live: {}, connecting: {}, queued: {}, seen: {}}}",
+         idx,
+         downloaded_pct,
+         SF::new(progress),
+         speed.download_mbps(),
+         SF::new(stats.fetched_bytes),
+         SF::new(stats.remaining_bytes),
+         SF::new(total),
+         SF::new(stats.uploaded_bytes),
+         peer_stats.live,
+         peer_stats.connecting,
+         peer_stats.queued,
+         peer_stats.seen,
+        );
+       },
+      }
+     }
+    });
     tokio::time::sleep(Duration::from_secs(1)).await;
    }
   }
