876eaf2a4ebd34ba8f70068dead64794f91df695
trevyn <trevyn-git@protonmail.com>
2021-10-31 20:45:18 -0400

    update rqbit, remove librclone

--- src/server.rs
+++ src/server.rs
@@ -51,9 +51,9 @@ async fn do_torrent() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
 
  let torrent_path = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent";
 
- let handle = match session.add_torrent(torrent_path.to_string(), Some(torrent_opts)).await? {
-  Some(handle) => handle,
-  None => return Ok(()),
+ let handle = match session.add_torrent(torrent_path, Some(torrent_opts)).await? {
+  librqbit::session::AddTorrentResponse::Added(handle) => handle,
+  _ => panic!("Unexpected response from session.add_torrent"),
  };
 
  librqbit::spawn_utils::spawn("Stats printer", {
